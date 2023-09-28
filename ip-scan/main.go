152bfc61e416e832915e54428c5dd53fd053b349
Julian Easterling <julian@julianscorner.com>
2023-09-28 19:52:07 -0400

    Think I have a potential solution for icmp on both linux and windows

--- ip-scan/main.go
+++ ip-scan/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"sort"
@@ -12,8 +11,6 @@ import (
 
 	"github.com/olekukonko/tablewriter"
 	"github.com/theckman/yacspin"
-	"golang.org/x/net/icmp"
-	"golang.org/x/net/ipv4"
 )
 
 type EndpointInfo struct {
@@ -151,48 +148,4 @@ func getFQDN(ip string) string {
 	}
 
 	return fqdn[0]
-}
-
-func pingIP(ip string) time.Duration {
-	addr := net.UDPAddr{IP: net.ParseIP(ip)}
-	conn, err := icmp.ListenPacket("udp4", "0.0.0.0")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer conn.Close()
-
-	msg := &icmp.Message{
-		Type: ipv4.ICMPTypeEcho,
-		Code: 0,
-		Body: &icmp.Echo{
-			ID:   os.Getpid() & 0xffff,
-			Seq:  0,
-			Data: []byte("ping"),
-		},
-	}
-
-	wb, _ := msg.Marshal(nil)
-	start := time.Now()
-	conn.WriteTo(wb, &addr)
-	rb := make([]byte, 1500)
-	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-	n, peer, err := conn.ReadFrom(rb)
-
-	if err == nil {
-		duration := time.Since(start)
-		rm, err := icmp.ParseMessage(1, rb[:n])
-		if err == nil {
-			if rm.Type == ipv4.ICMPTypeEchoReply {
-				echoReply, ok := msg.Body.(*icmp.Echo)
-				if ok {
-					if peer.(*net.UDPAddr).IP.String() == ip && echoReply.Seq == 0 {
-						return duration
-					}
-				}
-			}
-		}
-	}
-
-	return 0
 }
