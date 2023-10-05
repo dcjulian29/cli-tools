2cc73ff7ae4ae7896387c635d8fe8d1d73aa9241
Julian Easterling <julian@julianscorner.com>
2023-10-05 17:37:26 -0400

    Added a tool to sync git repositories simlar to the way the Powershell script did.

---
+++ git-reposync/go.mod
@@ -0,0 +1,16 @@
+module github.com/dcjulian29/cli-tools/git-reposync
+
+go 1.21.1
+
+require (
+	github.com/fatih/color v1.15.0
+	github.com/theckman/yacspin v0.13.12
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ git-reposync/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/theckman/yacspin"
+)
+
+type RepoInfo struct {
+	folder string
+	data   string
+}
+
+func main() {
+	pwd, _ := os.Getwd()
+
+	if len(os.Args) > 1 {
+		pwd = os.Args[1]
+	}
+
+	spinner, _ := yacspin.New(yacspin.Config{
+		Frequency: 100 * time.Millisecond,
+		Colors:    []string{"fgYellow"},
+		CharSet:   yacspin.CharSets[69],
+	})
+
+	spinner.Start()
+
+	var wg sync.WaitGroup
+	var directories []string
+
+	results := make(chan RepoInfo)
+	processed := make([]RepoInfo, 0)
+
+	err := filepath.Walk(pwd, func(path string, info os.FileInfo, err error) error {
+		if err == nil {
+			if info.IsDir() && info.Name() == ".git" {
+				directories = append(directories, filepath.Dir(path))
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, d := range directories {
+		wg.Add(1)
+
+		go func(d string) {
+			defer wg.Done()
+
+			results <- process(d)
+		}(d)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for item := range results {
+		processed = append(processed, item)
+	}
+
+	sort.Slice(processed, func(i, j int) bool {
+		return strings.Compare(processed[i].folder, processed[j].folder) < 0
+	})
+
+	spinner.Stop()
+
+	for _, info := range processed {
+		fmt.Printf("\n%s\n\n%s", color.HiYellowString(info.folder), info.data)
+	}
+}
+
+func process(path string) RepoInfo {
+	_ = executeGit(path, "fetch")
+
+	dirty := executeGit(path, "diff", "--stat")
+	local := executeGit(path, "rev-parse", "@")
+	remote := executeGit(path, "rev-parse", "@{u}")
+	base := executeGit(path, "merge-base", "@", "@{u}")
+	data := ""
+	pull := false
+	push := false
+	diverged := false
+
+	if local != remote {
+		if local == base {
+			pull = true
+		} else {
+			if remote == base {
+				push = true
+			} else {
+				diverged = true
+			}
+		}
+	}
+
+	if len(dirty) > 0 {
+		data = color.MagentaString("Skipping as it isn't clean...\n")
+	} else if diverged {
+		data = color.HiRedString("Skipping as it has diverged from upstream...\n")
+	} else {
+		if pull {
+			data = executeGit(path, "pull")
+		}
+
+		if push {
+			if len(data) > 0 {
+				data = fmt.Sprintf("%s\n\n%s", data, executeGit(path, "push"))
+			} else {
+				data = executeGit(path, "push")
+			}
+		}
+	}
+
+	if len(data) == 0 {
+		data = "No changes to push or pull...\n"
+	}
+
+	return RepoInfo{
+		folder: path,
+		data:   data,
+	}
+}
+
+func executeGit(path string, params ...string) string {
+	cmd := exec.Command("git", params...)
+	cmd.Dir = path
+
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return err.Error()
+	}
+
+	return string(out)
+}
