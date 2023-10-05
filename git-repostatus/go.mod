84770a0a7fc1b51d950291171193ad29b3e60088
Julian Easterling <julian@julianscorner.com>
2023-10-05 14:15:08 -0400

    Added a tool to replace a Powershell script of mine to show me the git repository status of multiple repositories.

---
+++ git-repostatus/go.mod
@@ -0,0 +1,17 @@
+module github.com/dcjulian29/cli-tools/git-repostatus
+
+go 1.21.1
+
+require (
+	github.com/fatih/color v1.15.0
+	github.com/olekukonko/tablewriter v0.0.5
+	github.com/theckman/yacspin v0.13.12
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ git-repostatus/main.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/olekukonko/tablewriter"
+	"github.com/theckman/yacspin"
+)
+
+type RepoInfo struct {
+	folder      string
+	dirty       bool
+	push_needed bool
+	pull_needed bool
+	diverged    bool
+}
+
+var (
+	Only_Dirty    bool
+	Only_Push     bool
+	Only_Pull     bool
+	Only_Diverged bool
+)
+
+func main() {
+	pwd, _ := os.Getwd()
+
+	if len(os.Args) > 1 {
+		pwd = os.Args[1]
+	}
+
+	if len(os.Args) > 2 {
+		switch os.Args[2] {
+		case "dirty":
+			Only_Dirty = true
+		case "push":
+			Only_Push = true
+		case "pull":
+			Only_Pull = true
+		case "diverged":
+			Only_Diverged = true
+		}
+	}
+
+	spinner, _ := yacspin.New(yacspin.Config{
+		Frequency: 100 * time.Millisecond,
+		Colors:    []string{"fgYellow"},
+		CharSet:   yacspin.CharSets[69],
+	})
+
+	spinner.Start()
+
+	var wg sync.WaitGroup
+	var directories []string
+
+	results := make(chan RepoInfo)
+	processed := make([]RepoInfo, 0)
+
+	err := filepath.Walk(pwd, func(path string, info os.FileInfo, err error) error {
+		if err == nil {
+			if info.IsDir() && info.Name() == ".git" {
+				directories = append(directories, filepath.Dir(path))
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, d := range directories {
+		wg.Add(1)
+
+		go func(d string) {
+			defer wg.Done()
+
+			info := process(d)
+
+			if Only_Dirty {
+				if info.dirty {
+					results <- info
+				}
+			} else if Only_Pull {
+				if info.pull_needed {
+					results <- info
+				}
+			} else if Only_Push {
+				if info.push_needed {
+					results <- info
+				}
+			} else if Only_Diverged {
+				if info.diverged {
+					results <- info
+				}
+			} else {
+				results <- info
+			}
+		}(d)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for item := range results {
+		processed = append(processed, item)
+	}
+
+	sort.Slice(processed, func(i, j int) bool {
+		return strings.Compare(processed[i].folder, processed[j].folder) < 0
+	})
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"PATH", "DIRTY", "PUSH", "PULL", "DIVERGED"})
+	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+	table.SetCenterSeparator("|")
+
+	for _, info := range processed {
+		table.Append([]string{
+			output_path(info),
+			output_dirty(info.dirty),
+			output_option(info.push_needed),
+			output_option(info.pull_needed),
+			output_option(info.diverged),
+		})
+	}
+
+	spinner.Stop()
+
+	table.Render()
+}
+
+func output_path(info RepoInfo) string {
+	if info.dirty {
+		return color.YellowString(info.folder)
+	}
+
+	if info.pull_needed || info.push_needed {
+		return color.RedString(info.folder)
+	}
+
+	return color.GreenString(info.folder)
+}
+
+func output_dirty(f bool) string {
+	if f {
+		return color.YellowString("yes")
+	}
+
+	return color.GreenString("no")
+
+}
+
+func output_option(f bool) string {
+	if f {
+		return color.RedString("yes")
+	}
+
+	return color.GreenString("no")
+
+}
+
+func process(path string) RepoInfo {
+	_ = executeExternalProgramCapture("git", path, "fetch")
+	dirty := executeExternalProgramCapture("git", path, "diff", "--stat")
+	local := executeExternalProgramCapture("git", path, "rev-parse", "@")
+	remote := executeExternalProgramCapture("git", path, "rev-parse", "@{u}")
+	base := executeExternalProgramCapture("git", path, "merge-base", "@", "@{u}")
+	pull := false
+	push := false
+	diverged := false
+
+	if local != remote {
+		if local == base {
+			pull = true
+		} else {
+			if remote == base {
+				push = true
+			} else {
+				diverged = true
+			}
+		}
+	}
+
+	return RepoInfo{
+		folder:      path,
+		dirty:       len(dirty) > 0,
+		push_needed: push,
+		pull_needed: pull,
+		diverged:    diverged,
+	}
+}
+
+func executeExternalProgramCapture(program string, path string, params ...string) string {
+	cmd := exec.Command(program, params...)
+	cmd.Stdin = os.Stdin
+	cmd.Dir = path
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return string(out)
+}
