ef594e75131e37d29d60e6687fedfa299b84ada2
Julian Easterling <julian@julianscorner.com>
2023-12-20 21:02:54 -0500

    Added a "global" build script so that it is easier to test everything before release

---
+++ build.ps1
@@ -0,0 +1,55 @@
+trap [System.Exception] {
+  "Exception: {0}" -f $_.Exception.Message
+  return
+}
+
+$baseDir = (Resolve-Path $("$PSScriptRoot")).Path
+$artifactDir = Join-Path -Path $baseDir -ChildPath "dist"
+
+if (Test-Path $artifactDir) {
+  Remove-Item -Path $artifactDir -Recurse -Force
+}
+
+New-Item -Path $artifactDir -ItemType Directory | Out-Null
+
+#------------------------------------------------------------------------------
+
+Get-ChildItem -Path $baseDir -Directory | ForEach-Object {
+  Push-Location $(Join-Path -Path $baseDir -ChildPath $_.Name)
+
+  if ((Test-Path ./.goreleaser.yml) -or (Test-Path ./.goreleaser.yaml)) {
+    if (Get-Command -Name goreleaser -ErrorAction SilentlyContinue) {
+      Write-Output "---------------------->  $pwd"
+      goreleaser build --single-target --clean --snapshot
+      Write-Output "`n`n"
+    }
+  }
+
+  Pop-Location
+}
+
+#------------------------------------------------------------------------------
+
+Get-ChildItem -Path $baseDir -Directory | ForEach-Object {
+  Push-Location $(Join-Path -Path $baseDir -ChildPath $_.Name)
+
+  if (Test-Path dist/artifacts.json) {
+    $dist = Get-Content -Path dist/artifacts.json -Raw | ConvertFrom-Json
+
+    foreach ($item in $dist) {
+      if ($item.type -eq "Binary") {
+        $src = Join-Path -Path $PWD -ChildPath $item.path
+        $dst = Join-Path -Path $artifactDir -ChildPath $item.name
+
+        Write-Output "$src --> $dst"
+
+        Move-Item -Path $src -Destination $dst
+      }
+    }
+
+    Remove-Item -Path dist/ -Recurse -Force
+  }
+
+
+  Pop-Location
+}
