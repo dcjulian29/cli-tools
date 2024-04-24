39be88c64926ada67ea0489c9e79e370fe20c257
Julian Easterling <julian@julianscorner.com>
2024-04-23 21:12:56 -0400

    Implemented Archive function and made it so you can specify directly which build tool to use

--- build-project/main.go
+++ build-project/main.go
@@ -4,116 +4,184 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/fatih/color"
 )
 
 func main() {
-	action := "build"
+	action := ""
 
 	if len(os.Args) > 1 {
 		action = os.Args[1]
+		action = strings.ReplaceAll(action, "-", "")
+	} else {
+		if fileExists("ansible.cfg") {
+			action = "ansible"
+		}
+
+		if fileExists("dockerfile") {
+			action = "docker"
+		}
+
+		if fileExists("go.mod") {
+			action = "go"
+		}
+
+		if fileExists(".goreleaser.yml") || fileExists(".goreleaser.yaml") {
+			action = "goreleaser"
+		}
+
+		if fileExists("build.sh") {
+			action = "sh"
+		}
+
+		if fileExists("build.bat") {
+			action = "bat"
+		}
+
+		if fileExists("build.cmd") {
+			action = "cmd"
+		}
+
+		if fileExists("build.ps1") {
+			action = "ps"
+		}
+
+		if fileExists("build.cake") {
+			action = "cake"
+		}
 	}
 
-	if strings.EqualFold(action, "archive") {
+	switch action {
+	case "archive":
 		archive()
 		os.Exit(0)
-	}
-
-	if fileExists("build.cake") {
+	case "cake":
 		buildCake()
 		os.Exit(0)
-	}
-
-	if fileExists("build.ps1") {
+	case "ps":
+	case "pshell":
+	case "powershell":
 		buildPowershell()
 		os.Exit(0)
-	}
-
-	if fileExists("build.bat") {
+	case "bat":
 		buildDos(true)
 		os.Exit(0)
-	}
-
-	if fileExists("build.cmd") {
+	case "cmd":
 		buildDos(false)
 		os.Exit(0)
-	}
-
-	if fileExists("build.sh") {
+	case "sh":
 		buildBash()
 		os.Exit(0)
-	}
-
-	if fileExists(".goreleaser.yml") || fileExists(".goreleaser.yaml") {
+	case "goreleaser":
 		buildGoReleaser()
 		os.Exit(0)
-	}
-
-	if fileExists("go.mod") {
+	case "go":
 		buildGo()
 		os.Exit(0)
-	}
-
-	if fileExists("ansible.cfg") {
+	case "ansible":
 		buildAnsible()
 		os.Exit(0)
-	}
-
-	if fileExists("dockerfile") {
+	case "docker":
 		buildDocker()
 		os.Exit(0)
+	case "":
+	default:
+		fmt.Println(color.RedString("Nothing found to build!"))
+		os.Exit(1)
 	}
-
-	fmt.Println(color.RedString("Nothing found to build!"))
 }
 
 func archive() {
-	// To Do
+	pwd, _ := os.Getwd()
+	name := filepath.Base(pwd)
+
+	dst := fmt.Sprintf("../%s.7z", name)
+
+	fmt.Printf("Archiving '%s'...\n", pwd)
+
+	run("7z", []string{"a", "-t7z", "-mx9", "-y", "-r", dst, "."})
 }
 
 func buildAnsible() {
-	run("ansible-lint", ".")
+	run("ansible-lint", []string{"."})
 }
 
 func buildBash() {
-	run("bash", "build.sh")
+	run("bash", []string{"build.sh"})
 }
 
 func buildCake() {
-	if len(os.Args) == 1 {
-		run("dotnet", "cake", "--target="+os.Args[0])
+	cmd := exec.Command("dotnet", "tool", "list")
+	tools, err := cmd.CombinedOutput()
+
+	if err != nil {
+		fmt.Println(color.RedString("dotnet SDK is not present!s"))
+		os.Exit(2)
 	}
 
-	var params = strings.Join(os.Args, " ")
+	if !strings.Contains(string(tools), "cake.tool") {
+		if !fileExists(".config/dotnet-tools.json") {
+			cmd := exec.Command("dotnet", "new", "tool-manifest")
+			_, err := cmd.CombinedOutput()
 
-	run("dotnet", "cake", params)
+			if err != nil {
+				fmt.Println(color.RedString("Installing Cake.Tool: %s", err))
+				os.Exit(3)
+			}
+
+			err = run("dotnet", []string{"tool", "install", "Cake.Tool"})
+
+			if err != nil {
+				fmt.Println(color.RedString("Cake.Tool is not present and could not be installed!"))
+				os.Exit(4)
+			}
+		}
+	}
+
+	var params []string
+
+	if len(os.Args) > 0 {
+		if os.Args[0] == "cake" {
+			if !strings.Contains("-", os.Args[1]) {
+				params = []string{"--target=" + os.Args[1]}
+			} else {
+				params = os.Args[1:]
+			}
+		} else {
+			params = os.Args
+		}
+	}
+
+	params = append([]string{"cake"}, params...)
+	run("dotnet", params)
 }
 
 func buildDocker() {
-	run("docker", "build", ".")
+	run("docker", []string{"build", "."})
 }
 
 func buildDos(batchFile bool) {
 	if batchFile {
-		run("cmd.exe", "/C", "build.bat")
+		run("cmd.exe", []string{"/C", "build.bat"})
 	} else {
-		run("cmd.exe", "/C", "build.cmd")
+		run("cmd.exe", []string{"/C", "build.cmd"})
 	}
 }
 
 func buildGoReleaser() {
-	run("goreleaser", "release", "--snapshot", "--clean")
+	run("goreleaser", []string{"release", "--snapshot", "--clean"})
 }
 
 func buildGo() {
-	run("go", "mod", "tidy")
-	run("go", "build", "-a", ".")
+	run("go", []string{"mod", "tidy"})
+	run("go", []string{"build", "-a", "."})
 }
 
 func buildPowershell() {
-	run("pwsh", "-f", "build.ps1")
+	run("pwsh", []string{"-f", "build.ps1"})
 }
 
 func fileExists(filename string) bool {
@@ -125,7 +193,7 @@ func fileExists(filename string) bool {
 	return !info.IsDir()
 }
 
-func run(binary string, params ...string) error {
+func run(binary string, params []string) error {
 	cmd := exec.Command(binary, params...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
